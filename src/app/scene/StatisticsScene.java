dbd2743c936a912fb4b82e31228c0744145e4523
Fraser McIntosh <frasermcintosh1@gmail.com>
2016-10-09 20:11:20 +1300

    Show percantage correct in stats screen

--- src/app/scene/StatisticsScene.java
+++ src/app/scene/StatisticsScene.java
@@ -1,12 +1,16 @@
 package app.scene;
 import app.AppModel;
+import app.model.FileModel;
+import app.model.LevelModel;
 import app.model.Statistics;
 
+import app.model.WordFile;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
@@ -29,15 +33,23 @@ public class StatisticsScene {
         
         //Create table layout for each level, in each of their own separate tabs
         for (int i = 1; i <= AppModel.getNumLevels(); i++) {
+           LevelModel level =  LevelModel.get(i-1);
         	//Create tab for level based on the current iteration number of for loop
             Tab tab = new Tab();
-            tab.setText("Level " + i);
-            
+            tab.setText(level + "");
+            int correct = FileModel.getWordsFromLevel(WordFile.MASTERED, level.getLevelAsInt()).size();
+            int incorrect = FileModel.getWordsFromLevel(WordFile.FAILED, level.getLevelAsInt()).size();
+            double percentage = ((correct + incorrect) != 0) ? ((correct * 100) / (correct + incorrect)) : -1;
+            Label lb = new Label();
+            String content = percentage == -1 ? "No words attempted" : percentage+ "";
+            lb.setText(content);
             //Create new instance of app.model.Statistics class passing level number into the object
             Statistics statsObject = new Statistics(i);
-            
+            VBox inner = new VBox();
+            inner.setAlignment(Pos.CENTER);
+            inner.getChildren().addAll(lb, statsObject.constructTableLayout());
             //Construct the table of words for current level and add to this level's tab pane
-            tab.setContent(statsObject.constructTableLayout());
+            tab.setContent(inner);
             tabPane.getTabs().add(tab);
         }
         
